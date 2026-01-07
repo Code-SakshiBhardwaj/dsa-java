e0bb1fa681c9719e885fbb0ef2a0ff2c0c31b160
I770179 <sakshi.bhardwaj@sap.com>
2026-01-07 14:25:02 +0530

    Added leftRotateByDPlaces, moveZerosToEnd, UnionOfTwoSortedArrays, IntersectionOfTwoSortedArrays

--- src/array/Basic.java
+++ src/array/Basic.java
@@ -1,7 +1,6 @@
 package array;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class Basic {
 
@@ -83,5 +82,184 @@ public class Basic {
       }
     }
     return i+1;
+  }
+
+  public static int[] leftRotateByDPlacesBF(int []arr, int d){
+    int n = arr.length;
+    d = d % n;
+    int []temp = new int[d];
+    for(int i = 0; i < d; i++){
+      temp[i] = arr[i];
+    }
+
+    for(int i = d; i < n; i++){
+      arr[i-d] = arr[i];
+    }
+
+    for(int i = 0; i < d; i++){
+      arr[n-d+i] = temp[i];
+    }
+    return arr;
+  }
+
+  public static int[] leftRotateByDPlacesOptimal(int []arr, int d){
+    int n = arr.length;
+    d = d % n;
+    reverseArr(arr, 0, d-1);
+    reverseArr(arr, d, n-1);
+    reverseArr(arr, 0, n-1);
+    return arr;
+  }
+
+  public static int[] reverseArr(int []arr, int start, int end){
+    int m = start+(end-start+1)/2;
+    while(start!= m){
+      int temp = arr[start];
+      arr[start] = arr[end-start];
+      arr[end-start] = temp;
+      start++;
+    }
+    return arr;
+  }
+
+  public static ArrayList<Integer> moveZerosToEndBF(ArrayList<Integer> arr){
+    int n = arr.size();
+    List<Integer> temp = new ArrayList<>();
+    for(int i = 0; i < n; i++){
+      if(arr.get(i) != 0){
+        temp.add(arr.get(i));
+      }
+    }
+    int m = temp.size();
+    for(int i = 0; i < m; i++){
+      arr.set(i,temp.get(i));
+    }
+    for(int i = m; i < n; i++){
+      arr.set(i,0);
+    }
+    return arr;
+  }
+
+  public static ArrayList<Integer> moveZerosToEndOptimal(ArrayList<Integer> arr){
+    int n = arr.size();
+    int j = -1;
+    for(int i = 0; i < n; i++){
+      if(arr.get(i)==0){
+        j = i;
+        break;
+      }
+    }
+    if(j==-1) return arr;
+
+    for(int i = j+1; i < n; i++){
+      if(arr.get(i) != 0){
+        int temp = arr.get(i);
+        arr.set(i,arr.get(j));
+        arr.set(j,temp);
+        j++;
+      }
+    }
+    return arr;
+  }
+
+  public static int linearSearch(ArrayList<Integer> arr, int value){
+    int n = arr.size();
+    for(int i = 0; i < n; i++){
+      if(arr.get(i) == value){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static ArrayList<Integer> UnionOfTwoSortedArraysBF(ArrayList<Integer> arr1, ArrayList<Integer> arr2){
+    HashSet<Integer> set = new HashSet<>();
+    for(int i = 0; i < arr1.size(); i++){
+      set.add(arr1.get(i));
+    }
+    for(int i = 0; i < arr2.size(); i++){
+      set.add(arr2.get(i));
+    }
+    ArrayList<Integer> union = new ArrayList<>();
+    for(int val:set){
+      union.add(val);
+    }
+    return union;
+  }
+
+  public static ArrayList<Integer> UnionOfTwoSortedArraysOptimal(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+    int n = arr1.size();
+    int m = arr2.size();
+    int i = 0;
+    int j = 0;
+    ArrayList<Integer> union = new ArrayList<>();
+    while (i < n && j < m) {
+      if (arr1.get(i) <= arr2.get(j)) {
+        if (union.size() == 0 || union.get(union.size() - 1) != arr1.get(i)) {
+          union.add(arr1.get(i));
+        }
+        i++;
+      } else {
+        if (union.size() == 0 || union.get(union.size() - 1) != arr2.get(j)) {
+          union.add(arr2.get(j));
+        }
+        j++;
+      }
+    }
+    while(i < n){
+      if (union.size() == 0 || union.get(union.size() - 1) != arr1.get(i)) {
+        union.add(arr1.get(i));
+      }
+      i++;
+    }
+    while(j < m){
+      if (union.size() == 0 || union.get(union.size() - 1) != arr2.get(j)) {
+        union.add(arr2.get(j));
+      }
+      j++;
+    }
+    return union;
+  }
+
+  public static List<Integer> IntersectionOfTwoSortedArraysBF(int []arr1, int []arr2){
+    int n1 = arr1.length;
+    int n2 = arr2.length;
+    List<Integer> intersection = new ArrayList<>();
+    List<Integer> visited = new ArrayList<>(Collections.nCopies(n2,0));
+    for(int i = 0; i < n1; i++){
+      for(int j = 0; j < n2; j++){
+        if(arr1[i] == arr2[j] && visited.get(j) == 0){
+          intersection.add(arr2[j]);
+          visited.set(j,1);
+          break;
+        }
+        if(arr1[i]<arr2[j]){
+          break;
+        }
+      }
+    }
+    return intersection;
+  }
+
+  public static List<Integer> IntersectionOfTwoSortedArraysOptimal(int []arr1, int []arr2){
+    int n1 = arr1.length;
+    int n2 = arr2.length;
+    List<Integer> intersection = new ArrayList<>();
+    int i = 0;
+    int j = 0;
+    while(i < n1 && j < n2){
+      if(arr1[i] < arr2[j]){
+        i++;
+      }
+      else if(arr1[i] > arr2[j]){
+        j++;
+      }
+      else{
+        intersection.add(arr1[i]);
+        i++;
+        j++;
+      }
+    }
+    return intersection;
   }
 }
--- src/array/Main.java
+++ src/array/Main.java
@@ -1,10 +1,18 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static array.Basic.*;
 
 public class Main {
+
+  public static void printArr(int []arr){
+    for(int j : arr){
+      System.out.print(j + " ");
+    }
+    System.out.println();
+  }
 
   public static void main(String []args){
     System.out.println(secondLargestBruteForce(new int[]{2,3,1,5,4}));
@@ -13,5 +21,19 @@ public class Main {
     System.out.println(isSorted(new int[]{1,2,3,4,5}));
     System.out.println(removeDuplicatesInPlaceFromSortedArrayBF(new int[]{1,1,2,2,3,4,4,5}));
     System.out.println(removeDuplicatesInPlaceFromSortedArrayOptimal(new int[]{1,1,2,2,3,4,4,5}));
+    printArr(leftRotateByDPlacesBF(new int[]{1,2,3,4,5}, 2));
+    printArr(leftRotateByDPlacesOptimal(new int[]{1,2,3,4,5},4));
+    ArrayList<Integer> arr = new ArrayList<>(List.of(1,2,0,3,0,4,5));
+//    moveZerosToEndBF(arr);
+//    System.out.println(linearSearch(arr, 4));
+//    moveZerosToEndOptimal(arr);
+//    System.out.println(arr);
+    ArrayList<Integer> arr1 = new ArrayList<>(List.of(1,2,4,5));
+    ArrayList<Integer> arr2 = new ArrayList<>(List.of(1,2,3,3,6,7,9,10));
+//    System.out.println(UnionOfTwoSortedArraysBF(arr1,arr2));
+    System.out.println(UnionOfTwoSortedArraysOptimal(arr1,arr2));
+//    System.out.println(IntersectionOfTwoSortedArraysBF(new int[]{1,2,2,3,4}, new int[]{2,2,3,5}));
+    System.out.println(IntersectionOfTwoSortedArraysOptimal(new int[]{1,2,2,3,4}, new int[]{2,2,3,5}));
+
   }
 }
